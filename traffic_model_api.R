5315b01c2a1d0695c8de6674bc438abe20c7aa2b
Shivraj-Pun <118063371+Shivraj-Pun@users.noreply.github.com>
2025-05-27 15:32:07 +0530

    Update traffic_model_api.R

--- traffic_model_api.R
+++ traffic_model_api.R
@@ -1,21 +1,16 @@
 library(plumber)
 source("traffic_model_logic.R")
 
-
-
-
 function(day, hour) {
   pred <- predict_traffic(model_info, day, as.numeric(hour))
   list(prediction = round(pred * 100, 1))
 }
-
 
 function() {
   traffic_data <- prepare_data()
   model_info <- train_model(traffic_data)
   return("Model trained and data prepared")
 }
-
 
 function() {
   traffic_data
